ab0e944dfd315b24229a78236ad06f3e70a42d74
andreisdc <101742390+andreisdc@users.noreply.github.com>
2023-05-21 00:45:50 +0300

    Add Commands

--- ProiectMVP/MainWindow.xaml.cs
+++ ProiectMVP/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ProiectMVP.ViewModel;
 
 namespace ProiectMVP
 {
@@ -23,6 +24,7 @@ namespace ProiectMVP
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
     }
 }
---
+++ ProiectMVP/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ProiectMVP.Command.ChangeViewCommand;
+
+namespace ProiectMVP.ViewModel
+{
+	public class MainWindowViewModel: BaseViewModel
+	{
+		private BaseViewModel _selectedViewModel;
+
+		public BaseViewModel SelectedViewModel
+		{
+			get { return _selectedViewModel;}
+			set { _selectedViewModel = value; }
+		}
+
+		public ICommand ChangeView { get; set; }
+
+		public MainWindowViewModel()
+		{
+			ChangeView = new ChangeViewCommand(this);
+		}
+
+	}
+}
