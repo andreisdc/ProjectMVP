11cc0e015723503a8d9ea22289b64015b5f98153
andreisdc <101742390+andreisdc@users.noreply.github.com>
2023-05-23 00:36:14 +0300

    Start ClassVM Function

--- ProiectMVP/ViewModels/AdminViewModel.cs
+++ ProiectMVP/ViewModels/AdminViewModel.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+
 
 namespace ProiectMVP.ViewModels
 {
     internal class AdminViewModel : BaseViewModel
     {
+	    public AdminViewModel()
+	    {
+	    }
 
-    }
+	}
 }
--- ProiectMVP/ViewModels/PageViewModels/Admin/SubjectVM.cs
+++ ProiectMVP/ViewModels/PageViewModels/Admin/SubjectVM.cs
@@ -4,16 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ProiectMVP.ViewModels.PageViewModels.Admin
 {
 	class SubjectVM
 	{
+
+		public ICommand EditSubjectModeCommand { get; }
+		public ICommand DeleteSubjectModeCommand { get; }
+		public ICommand InsertSubjectModeCommand { get; }
+
+
 		public readonly PageModel _pageModel;
 
 		public SubjectVM()
 		{
 			_pageModel = new PageModel();
+			EditSubjectModeCommand = new RelayCommand(EditSubjectMode);
+			DeleteSubjectModeCommand = new RelayCommand(DeleteSubjectMode);
+			InsertSubjectModeCommand = new RelayCommand(InsertSubjectMode);
+		}
+
+		public void EditSubjectMode(object parameter)
+		{
+
+		}
+		public void DeleteSubjectMode(object parameter)
+		{
+
+		}
+		public void InsertSubjectMode(object parameter)
+		{
+
 		}
 	}
 }
