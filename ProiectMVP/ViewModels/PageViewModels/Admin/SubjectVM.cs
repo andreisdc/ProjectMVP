016d4a3f42839b782ae9b7a5ae2d78a3217ef626
andreisdc <101742390+andreisdc@users.noreply.github.com>
2023-05-22 18:58:14 +0300

    Add Navigation

---
+++ ProiectMVP/ViewModels/PageViewModels/Admin/SubjectVM.cs
@@ -0,0 +1,19 @@
+﻿using ProiectMVP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectMVP.ViewModels.PageViewModels.Admin
+{
+	class SubjectVM
+	{
+		public readonly PageModel _pageModel;
+
+		public SubjectVM()
+		{
+			_pageModel = new PageModel();
+		}
+	}
+}
